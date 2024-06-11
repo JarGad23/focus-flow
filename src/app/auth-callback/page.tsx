68a60bdabe3d54c47f071797a029782448c45182
JarGad23 <j.gad@m8b.pl>
2024-06-11 18:09:04 +0200

    Implemented auth callback

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { Loader2 } from "lucide-react";
+import { getAuthStatus } from "./actions";
+import { useRouter } from "next/navigation";
+
+const AuthCallbackPage = () => {
+  const router = useRouter();
+  const { data } = useQuery({
+    queryKey: ["auth-callback"],
+    queryFn: async () => getAuthStatus(),
+    retry: true,
+    retryDelay: 500,
+  });
+
+  if (data?.success) {
+    router.push("/");
+  }
+
+  return (
+    <div className="w-full mt-24 flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-500" />
+        <h3 className="font-semibold text-xl">Logging you in...</h3>
+        <p>You will be redirected automatically.</p>
+      </div>
+    </div>
+  );
+};
+
+export default AuthCallbackPage;
