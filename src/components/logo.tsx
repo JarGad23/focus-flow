560268fc21a1ceea2221457ede4d553b3e4e12c8
JarGad23 <j.gad@m8b.pl>
2024-06-12 21:32:39 +0200

    Added reusable logo component

---
+++ src/components/logo.tsx
@@ -0,0 +1,8 @@
+export const Logo = () => {
+  return (
+    <h3 className="text-xl">
+      <span className="text-primary">F</span>ocus
+      <span className="text-primary">F</span>low
+    </h3>
+  );
+};
