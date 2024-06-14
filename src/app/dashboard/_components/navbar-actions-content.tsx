8a3b60f05810bd46d30c2a089892e9e55f8a6ed6
Jarosław Gad <127743272+JarGad23@users.noreply.github.com>
2024-06-14 14:56:14 +0200

    Feature 8: Dashboard navbar actions (#14)
    * Pacakge json changes
    
    * Css class
    
    * Layout changes
    
    * Added shadcn components
    
    * Developed navbar actions

---
+++ src/app/dashboard/_components/navbar-actions-content.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { Button, buttonVariants } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { cn } from "@/lib/utils";
+import {
+  timeFormatType,
+  timePeriodType,
+  useTimePeriod,
+} from "@/store/useTimePeriod";
+import Link from "next/link";
+
+type Props = {
+  isPro: boolean;
+  className?: string;
+};
+
+export const NavbarActionsContent = ({ isPro, className }: Props) => {
+  const { onTimeFormatChange, timeFormat, timePeriod, onTimePeriodChange } =
+    useTimePeriod();
+
+  return (
+    <div className={cn("w-full flex items-center gap-x-2", className)}>
+      <Select
+        value={timePeriod}
+        onValueChange={(value: timePeriodType) => onTimePeriodChange(value)}
+      >
+        <SelectTrigger className="w-full md:w-[100px] focus:ring-offset-0 focus:ring-transparent outline-none">
+          <SelectValue placeholder="Time Period" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="DAY">Day</SelectItem>
+          <SelectItem value="WEEK">Week</SelectItem>
+          <SelectItem value="MONTH">Month</SelectItem>
+        </SelectContent>
+      </Select>
+      <Select
+        value={timeFormat}
+        onValueChange={(value: timeFormatType) => onTimeFormatChange(value)}
+      >
+        <SelectTrigger className="w-full md:w-[100px] focus:ring-offset-0 focus:ring-transparent outline-none">
+          <SelectValue placeholder="Time Format" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="24H">24 hour</SelectItem>
+          <SelectItem value="12H">12 hour</SelectItem>
+        </SelectContent>
+      </Select>
+      <Button className="w-full md:w-fit">Become Pro!</Button>
+      <Link
+        href="/api/auth/logout"
+        className={cn(
+          buttonVariants({
+            variant: "ghost",
+            size: "sm",
+            className: "w-full md:w-fit",
+          })
+        )}
+      >
+        Logout
+      </Link>
+    </div>
+  );
+};
