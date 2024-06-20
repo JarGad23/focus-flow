6f80cc7cc9bd133a4adbc02841d9f842b9c021bc
JarGad23 <j.gad@m8b.pl>
2024-06-20 21:43:31 +0200

    Added form schema and utils

--- src/schemas/create-form-schema.ts
+++ src/schemas/create-form-schema.ts
@@ -1,5 +1,28 @@
 import { z } from "zod";
 
-const createFormSchema = z.object({
-  title: z.string(),
+export const StatusEnum = z.enum(["incompleted", "inProgress", "completed"]);
+export const PriorityEnum = z.enum(["low", "medium", "high"]);
+
+const TaskSchema = z.object({
+  id: z.string().optional(),
+  title: z.string().min(1, { message: "Title is required" }),
+  description: z.string().optional(),
+  day: z.date(),
+  startTime: z.date(),
+  endTime: z.date(),
+  status: StatusEnum.default("incompleted"),
+  priority: PriorityEnum.default("medium"),
+  tagId: z.string().optional(),
 });
+
+const EventSchema = z.object({
+  id: z.string().optional(),
+  title: z.string().min(1, "Title is required"),
+  description: z.string().optional(),
+  isAllDay: z.boolean().default(false),
+  day: z.date(),
+  month: z.number().int(),
+  year: z.number().int(),
+});
+
+export { TaskSchema, EventSchema };
