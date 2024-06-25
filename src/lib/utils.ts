62ce6d7847158f387ee89917c905e631a72a13fb
JarGad23 <j.gad@m8b.pl>
2024-06-25 15:12:48 +0200

    Displayed tasks in day view, added helper to calculate task boundries

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,7 @@ import { format, addMinutes, set } from "date-fns";
 
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { Task } from "@prisma/client";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -25,4 +26,45 @@ export const generateTimeBlocks = (
     current = addMinutes(current, 15);
   }
   return Array.from(new Set(blocks));
+};
+
+export const getOverlappingTasks = (tasks: Task[]): Task[][] => {
+  const groupedTasks: Task[][] = [];
+
+  tasks.forEach((task) => {
+    let placed = false;
+
+    for (const group of groupedTasks) {
+      if (
+        group.some(
+          (t) =>
+            new Date(t.startTime) < new Date(task.endTime) &&
+            new Date(task.startTime) < new Date(t.endTime)
+        )
+      ) {
+        group.push(task);
+        placed = true;
+        break;
+      }
+    }
+
+    if (!placed) {
+      groupedTasks.push([task]);
+    }
+  });
+
+  return groupedTasks;
+};
+
+export const calculateTaskPosition = (task: Task) => {
+  const startHour = new Date(task.startTime).getHours();
+  const startMinute = new Date(task.startTime).getMinutes();
+  const endHour = new Date(task.endTime).getHours();
+  const endMinute = new Date(task.endTime).getMinutes();
+
+  const startPosition = startHour * 64 + (startMinute / 15) * 16;
+  const endPosition = endHour * 64 + (endMinute / 15) * 16;
+  const taskHeight = endPosition - startPosition;
+
+  return { startPosition, taskHeight };
 };
